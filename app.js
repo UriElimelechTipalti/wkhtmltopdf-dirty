6648e23b6c30ccb889fb7e77b69ffab29e633b1b
TIPALTI\uri.elimelech <uri.elimelech@tipalti.com>
2022-05-23 10:55:58 +0300

    using express

--- app.js
+++ app.js
@@ -1,21 +1,30 @@
-const http = require('http');
-
-const hostname = '127.0.0.1';
-const port = 3000;
+const express = require('express')
+const app = express()
+const port = 3000
 
 const print = s => {
     console.log(s)
 }
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 302;
-  print(res.statusCode)
+// const server = http.createServer((req, res) => {
+//   res.statusCode = 302;
+//   print(res.statusCode)
+//   res.setHeader('Location', 'file:///Windows/system32/drivers/etc/hosts');
+//   res.end('Hello World');
+// });
+
+// server.listen(port, hostname, () => {
+//   console.log(`Server running at http://${hostname}:${port}/`);
+// });
+
+app.get('/', (req,res) => {
+  res.statusCode = 302
   res.setHeader('Location', 'file:///Windows/system32/drivers/etc/hosts');
   res.end('Hello World');
-});
+})
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+app.listen(port, () => {
+  print("example")
+})
 
 // document.write(`<iframe src="http://"></iframe>`)
